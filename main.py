9601cc742898880b130b6eff331bcd23d6989c51
ibtissam01 <89752387+ibtissam01@users.noreply.github.com>
2023-05-25 02:45:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
