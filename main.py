712d4088e423b85c61f21393519fe50e94361556
ibtissam01 <89752387+ibtissam01@users.noreply.github.com>
2023-05-25 03:41:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,24 +15,8 @@ chart_data = pd.DataFrame(
 st.line_chart(chart_data)
 
 
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-
-st.map(map_data)
 
 
-left_column, right_column = st.columns(2)
-# You can use a column just like st.sidebar:
-left_column.button('Press me!')
-
-# Or even better, call Streamlit functions inside a "with" block:
-with right_column:
-    chosen = st.radio(
-        'Sorting hat',
-        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
-    st.write(f"You are in {chosen} house!")
-    
 
 
 # Charger les données à partir d'un fichier CSV
