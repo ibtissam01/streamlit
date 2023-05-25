0ef351617555981a61d32d701d6c8d40fbf70887
ibtissam01 <89752387+ibtissam01@users.noreply.github.com>
2023-05-25 02:46:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
