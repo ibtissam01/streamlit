7bae536cb1cbe57d64253aa1fc4ac0023b5a19d7
ibtissam01 <89752387+ibtissam01@users.noreply.github.com>
2023-05-25 04:33:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,4 +22,20 @@ st.pyplot(fig)
 
 # Créer une visualisation de nuage de points pour la relation entre la quantité et le prix de vente
 st.write("Relation entre la quantité et le prix de vente :")
-fig, ax = pltDésolé, il semble que j'ai accidentellement envoyé une réponse incomplète. Voici la suite de ma réponse :
+fig, ax = plt.subplots()
+sns.scatterplot(x="QUANTITY", y="PRICE", data=sales_data, ax=ax)
+st.pyplot(fig)
+st.write("Modèle de régression linéaire pour prédire les ventes futures :")
+X = sales_data[["QUANTITY"]]
+y = sales_data["SALES"]
+model = LinearRegression()
+model.fit(X, y)
+quantity_input = st.number_input("Entrez la quantité vendue :", min_value=0, max_value=10000)
+predicted_sales = model.predict([[quantity_input]])
+st.write("Les ventes prévues pour la quantité vendue sont de :", predicted_sales)
+st.write("Modèle de clustering pour identifier les meilleures ventes :")
+X = sales_data[["QUANTITY", "PRICE"]]
+model = KMeans(n_clusters=3)
+model.fit(X)
+sales_data["cluster"] = model.predict(X)
+st.write(sales_data.groupby("cluster").mean())
