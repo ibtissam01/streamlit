0c022b172629e0bacc688e86cb22a4132ae19753
ibtissam01 <89752387+ibtissam01@users.noreply.github.com>
2023-05-25 04:12:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 
 # Charger les données à partir d'un fichier CSV
@@ -10,7 +10,7 @@ sales_data = pd.read_csv("https://raw.githubusercontent.com/ibtissam01/streamlit
 st.write(sales_data.shape)
 st.write(sales_data.dtypes)
 st.write(sales_data.describe())
-
+st.pyplot(sns.histplot(sales_data["QUANTITY"]))
 
 # Ajouter un filtre pour afficher les données par type de vente
 sale_types = sales_data["SALE TYPE"].unique()
