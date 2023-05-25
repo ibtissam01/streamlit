b5511ef577f2ee7b7610d1597154294714e2dc17
ibtissam01 <89752387+ibtissam01@users.noreply.github.com>
2023-05-25 04:31:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.cluster import KMeans
 
 # Charger les données à partir d'un fichier CSV
-sales_data = pd.read_csv("https://raw.githubusercontent.com/ibtissam01/streamlit/main/sales_data.csv")
+sales_data = pd.read_csv("sales_data.csv")
 
 # Afficher les informations générales sur les données
+st.write("Informations générales sur les données :")
 st.write(sales_data.shape)
 st.write(sales_data.dtypes)
 st.write(sales_data.describe())
 
+# Créer une visualisation d'histogramme pour la distribution des quantités vendues
+st.write("Distribution des quantités vendues :")
 fig, ax = plt.subplots()
 sns.histplot(sales_data["QUANTITY"], ax=ax)
 st.pyplot(fig)
-# Ajouter un filtre pour afficher les données par type de vente
-sale_types = sales_data["SALE TYPE"].unique()
-selected_sale_type = st.selectbox("Select a sale type", sale_types)
-sale_type_data = sales_data[sales_data["SALE TYPE"] == selected_sale_type]
-st.write(sale_type_data)
+
+# Créer une visualisation de nuage de points pour la relation entre la quantité et le prix de vente
+st.write("Relation entre la quantité et le prix de vente :")
+fig, ax = pltDésolé, il semble que j'ai accidentellement envoyé une réponse incomplète. Voici la suite de ma réponse :
